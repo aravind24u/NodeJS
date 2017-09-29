f24bdabf8c6edd5c2539c1466d6e74ad1f58a486
aravind24u <s.aravind4u@gmail.com>
2017-09-30 02:51:44 +0530

    Adding ex-1

---
+++ ex-1/Callbacks.js
@@ -0,0 +1,22 @@
+var placeOrder = function(orderNumber,seconds){
+  console.log('Taking up orderNumber : ' ,orderNumber);
+  processOrder(orderNumber,seconds,function(){
+    console.log('Processed Order : ' , orderNumber);
+    console.log('delivering order : ' , orderNumber);
+  });
+
+}
+
+var processOrder = function(orderNumber,seconds,callback){
+  console.log('Procesing Order : ' , orderNumber);
+  setTimeout(callback,seconds);
+
+}
+
+
+
+placeOrder(2,5000);
+placeOrder(3,4000);
+placeOrder(4,3000);
+placeOrder(5,2000);
+placeOrder(6,1000);
